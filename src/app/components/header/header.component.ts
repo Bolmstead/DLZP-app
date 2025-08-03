8134d36d81badff6dfcb4dc7a78427aaccfcb081
BerkleyDEV <88558484+BerkleyDEV@users.noreply.github.com>
2025-08-03 10:37:28 -0600

    .

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,21 @@
 import { Component, signal } from '@angular/core';
+import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   template: `
-    <div class="bg-slate-100 px-4 py-3 shadow-md">
+    <div
+      class="bg-slate-100 px-4 py-3 shadow-md flex justify-between items-center"
+    >
       Berkley's DLZP Angular App
+      <app-primary-button label="Login" (btnClicked)="btnClick()" />
     </div>
   `,
   styles: ``,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  btnClick() {
+    console.log('button clicked');
+  }
+}
--- src/app/components/primary-button/primary-button.component.ts
+++ src/app/components/primary-button/primary-button.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-primary-button',
   imports: [],
   template: `
-    <p>
-      primary-button works!
-    </p>
+    <button
+      class="bg-blue-500 text-white px-4 py-2 rounded-md"
+      (click)="handleButtonClick()"
+    >
+      {{ label() }}
+    </button>
   `,
-  styles: ``
+  styles: ``,
 })
 export class PrimaryButtonComponent {
+  label = input.required<string>();
 
+  btnClicked = output<void>();
+
+  handleButtonClick() {
+    console.log('Button clicked');
+    this.btnClicked.emit();
+  }
 }
